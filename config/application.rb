a4693bee025ee388629f3333bdaf1ad0fbb5c1f1
zaborenkodm <zaborenkodm@gmail.com>
2017-02-01 17:50:02 +0300

    Localization

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
   end
 end
